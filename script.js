1d234a65340a2eef7ff9afd0c188069d8a591f78
aadyakeni <118092960+aadyakeni@users.noreply.github.com>
2023-10-21 18:23:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,26 @@ let weather ={
     "apiKey": "ff6104d4c2a6c5fb7a855ec77312599e",
 
     fetchWeather: function (city) {
+
+        //fetches api data for a given city
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
         + city 
         + "&units=metric&APPID=" 
         + this.apiKey
         )
-        .then((response)=> response.json())
-        .then((data) => this.displayWeather(data));
+        .then((response)=> response.json()) //converts response data to JSON format
+        .then((data) => this.displayWeather(data)); //passes the data to the function
     },
 
     displayWeather: function(data){
+
+        //extracts informmation from the data
         const{ name } = data;
         const{ icon, description } = data.weather[0];
         const{ temp, humidity } = data.main;
         const{ speed }= data.wind;
 
+        //changes as per data recieved
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
@@ -25,20 +30,27 @@ let weather ={
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
 
+        //removes loading from class "weather loading"
         document.querySelector(".weather").classList.remove("loading");
+
+        //sets background to the image of the city entered
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
     },
 
     search: function(){
+
+        //passes the input inside searchbar to the function for weather
         this.fetchWeather(document.querySelector(".searchbar").value);
     }
 };
 
+//search when search button is pressed
 document.querySelector(".search button")
 .addEventListener(("click"), function(){
     weather.search();
 });
 
+//search when enter is pressed
 document.querySelector(".searchbar").addEventListener("keyup", function(event){
     if (event.key == "Enter") {
         weather.search();
